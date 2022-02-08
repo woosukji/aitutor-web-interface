87f93ac7b2b662283dc721e1fe05b38206f9f89b
woosukji <woosukji@snu.ac.kr>
2022-02-08 16:54:02 +0900

    add vertical card

---
+++ aitutor-web/src/components/CardVertical.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card :to="cardTo" max-width="300" elevation="0">
+    <v-img
+      class="white--text align-end rounded-lg"
+      height="200px"
+      :src="cardImageSrc"
+    >
+      <v-card-title>{{ cardTitle }}</v-card-title>
+    </v-img>
+
+    <v-card-text text-center class="font-weight-bold">
+      <slot> </slot>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "CardVertical",
+  props: {
+    cardTo: String,
+    cardTitle: String,
+    cardImageSrc: String,
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+};
+</script>
+
+<style scoped lang="scss"></style>
