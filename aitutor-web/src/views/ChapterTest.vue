acd6e13c3e017461df5282ef1f14ab7e32efd58f
woosukji <woosukji@snu.ac.kr>
2022-02-08 17:41:08 +0900

    set up chapter selecting part of "ChapterTest" page

--- aitutor-web/src/views/ChapterTest.vue
+++ aitutor-web/src/views/ChapterTest.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="chapter-test">
-    <h1>This is chapter test page</h1>
+  <div class="chapter-test d-flex justify-center align-center">
+    <chapter-selection
+      style="flex-grow: 1"
+      :chapter-list="chapterList"
+      @selected="handleSelected"
+    ></chapter-selection>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import ChapterSelection from "@/components/ChapterSelection.vue";
+
+export default {
+  name: "ChapterTest",
+  components: { ChapterSelection },
+  data() {
+    return {
+      chapterList: ["hi", "chapter", "list"],
+    };
+  },
+  computed: {},
+  methods: {
+    handleSelected(e) {
+      console.log(`selection recieved: ${e}`);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.chapter-test {
+  height: 100%;
+}
+</style>
