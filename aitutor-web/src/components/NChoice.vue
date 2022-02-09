0ba3977517438061d9eb99d69bf4cc7eee2c0595
woosukji <woosukji@snu.ac.kr>
2022-02-09 13:59:00 +0900

    add `NChoice` component
    n지선다 view component

---
+++ aitutor-web/src/components/NChoice.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="n-choice d-flex flex-column">
+    <div class="options d-flex flex-column">
+      <v-btn
+        v-for="(option, i) in optionList"
+        :key="i"
+        :disabled="choice === i"
+        @click="choice = i"
+        color="accent"
+        class="mb-2"
+        >{{ option }}</v-btn
+      >
+    </div>
+    <v-spacer></v-spacer>
+    <v-btn
+      class="chosen mt-4"
+      :disabled="choice === undefined"
+      @click="onChosen"
+      color="primary"
+      >done!</v-btn
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NChoice",
+  props: {
+    optionList: Array,
+  },
+  data() {
+    return {
+      choice: undefined,
+    };
+  },
+  computed: {},
+  methods: {
+    onChosen() {
+      this.$emit("chosen", this.choice);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
