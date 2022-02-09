0b39c60f49db2408556c2b37f29fff0570334fa3
woosukji <woosukji@snu.ac.kr>
2022-02-09 13:59:59 +0900

    set up problem solving step of `ChapterTest` page

--- aitutor-web/src/views/ChapterTest.vue
+++ aitutor-web/src/views/ChapterTest.vue
@@ -1,29 +1,129 @@
 <template>
   <div class="chapter-test d-flex justify-center align-center">
     <chapter-selection
+      v-if="mode === 'select-chapter'"
+      class="select-chapter"
       style="flex-grow: 1"
       :chapter-list="chapterList"
-      @selected="handleSelected"
+      @selected="handleChapterSelected"
     ></chapter-selection>
+
+    <v-container
+      v-else-if="mode == 'solve-problems'"
+      class="solve-problems grey lighten-5"
+    >
+      <v-row>
+        <v-col>
+          <v-progress-linear
+            :value="(nSolved / nProblems) * 100"
+            round
+            height="25"
+          >
+            <div class="font-weight-bold">
+              {{ `${nSolved} / ${nProblems}` }}
+            </div>
+          </v-progress-linear>
+        </v-col>
+      </v-row>
+
+      <v-row align="center">
+        <v-col>
+          <card-horizontal :card-image-src="currentProblem.imgSrc">
+            <div class="problem-text">{{ currentProblem.questionText }}</div>
+          </card-horizontal>
+        </v-col>
+
+        <v-spacer></v-spacer>
+
+        <v-col>
+          <n-choice
+            :option-list="currentProblem.optionList"
+            @chosen="handleProblemSolved"
+            :key="nSolved"
+          ></n-choice>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container v-else-if="mode == 'show-result'">
+      {{ choices }}
+    </v-container>
+
+    <v-overlay :value="loading">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import ChapterSelection from "@/components/ChapterSelection.vue";
+import CardHorizontal from "@/components/CardHorizontal.vue";
+import NChoice from "@/components/NChoice.vue";
 
 export default {
   name: "ChapterTest",
-  components: { ChapterSelection },
+  components: { ChapterSelection, CardHorizontal, NChoice },
   data() {
     return {
+      mode: "select-chapter",
+      loading: false,
       chapterList: ["hi", "chapter", "list"],
+      nSolved: 0,
+      nProblems: 3,
+      problems: [
+        {
+          imgSrc: "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
+          questionText: "1. 다음 그림에서 x의 값은?",
+          optionList: ["1", "2", "4", "8", "없다"],
+        },
+        {
+          imgSrc: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+          questionText: "2. 다음 그림에서 y의 값은?",
+          optionList: ["1", "2", "4", "9", "없다"],
+        },
+        {
+          imgSrc: "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
+          questionText: "3. 다음 그림에서 z의 값은?",
+          optionList: ["1", "2", "4", "10", "없다"],
+        },
+      ],
+      currentProblem: {},
+      choices: [],
     };
   },
   computed: {},
   methods: {
-    handleSelected(e) {
+    handleChapterSelected(e) {
       console.log(`selection recieved: ${e}`);
+
+      this.loadProblems();
+
+      this.mode = "solve-problems";
+    },
+    loadProblems() {
+      this.currentProblem = this.problems[this.nSolved];
+    },
+    async handleProblemSolved(e) {
+      const choice = e + 1;
+      this.nSolved += 1;
+
+      this.choices.push(choice);
+
+      if (this.nSolved < this.problems.length) {
+        this.currentProblem = this.problems[this.nSolved];
+      } else {
+        await this.loadResult();
+        this.loading = false;
+        this.mode = "show-result";
+      }
+    },
+    loadResult() {
+      this.loading = true;
+      return new Promise((resolve) => setTimeout(this.resolver, 1000, resolve));
+    },
+    resolver(resolve) {
+      this.loading = true;
+      resolve();
     },
   },
 };
