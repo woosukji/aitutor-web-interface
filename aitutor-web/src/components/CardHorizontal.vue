1e74969d4042637dc90b870f34683e30377de37a
woosukji <woosukji@snu.ac.kr>
2022-02-09 13:58:04 +0900

    add `CardHorizontal` component
    임시방편. 추후 `CardVertical` 과 기능상 병합 예정

---
+++ aitutor-web/src/components/CardHorizontal.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card max-width="300" elevation="0">
+    <v-img
+      class="white--text align-end rounded-lg"
+      height="200px"
+      :src="cardImageSrc"
+    >
+    </v-img>
+
+    <v-card-text text-center class="font-weight-bold">
+      <slot> </slot>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "CardHorizontal",
+  props: {
+    cardImageSrc: String,
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+};
+</script>
+
+<style scoped lang="scss"></style>
