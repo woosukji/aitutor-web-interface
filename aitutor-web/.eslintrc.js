d2a82ebd93aa17cbabbc28ba1154e6bb93c12af0
woosukji <woosukji@snu.ac.kr>
2022-02-10 17:18:09 +0900

    ignore ESLint curly newline option
    prettier 방식을 선호

--- aitutor-web/.eslintrc.js
+++ aitutor-web/.eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "linebreak-style": ["error", process.platform === "win32" ? "windows" : "unix"],
     quotes: ["off", "single"],
+    "object-curly-newline": "off",
   },
 };
