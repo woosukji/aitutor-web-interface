f08b631da665867b1650ba0bbe9245cb469b7ba3
woosukji <woosukji@snu.ac.kr>
2022-02-17 17:30:41 +0900

    enhance readability in `ProblemHelper` logic

--- aitutor-web/src/util/ProblemHelper.js
+++ aitutor-web/src/util/ProblemHelper.js
@@ -1,4 +1,5 @@
 const removeNumbering = (text) => {
+  /* text가 번호로 시작할 경우, 불필요한 넘버링이므로 제거 */
   const firstWord = text.split(/\s+/)[0];
   const numberingRemoved = !Number.isNaN(firstWord - parseFloat(firstWord))
     ? text.slice(firstWord.length)
@@ -8,12 +9,25 @@ const removeNumbering = (text) => {
 };
 
 const parseTabularForKatex = (text) => {
+  /*
+  데이터에서 OCR된 텍스트는 LaTeX 포맷으로, KaTeX와 충돌 발생
+  text에 표(tabular)를 포함할 경우, array로 변환하여 KaTeX가 인식하도록 전처리
+  */
+
+  /* "\begin{tabular} ~~ \end{tabular}" 형태 검색, 안쪽의 내용 (~~) 추출해 저장 */
   const pattern = /\\begin\{tabular\}([\s\S]+)\\end\{tabular\}/;
   const tabularSearched = pattern.exec(text);
 
   if (tabularSearched) {
+    /* tabular 내부에서 inline 표현 "$~~$" 사용 시 오류 발생하므로, 제거 */
     const tabularContentForKatex = tabularSearched[1].replaceAll("$", "");
-    const tabularForKatex = `$$\\begin{array}${tabularContentForKatex}\\end{array}$$`;
+    const tabularForKatex = `
+    $$
+    \\begin{array}
+    ${tabularContentForKatex}
+    \\end{array}
+    $$
+    `;
     return text.replace(pattern, tabularForKatex);
   }
 
@@ -21,10 +35,8 @@ const parseTabularForKatex = (text) => {
 };
 
 const parseQuestionText = (questionText) => {
-  /* text가 번호로 시작할 경우, 불필요한 넘버링이므로 제거 */
   const numberingRemoved = removeNumbering(questionText);
 
-  /* text에 표(tabular)를 포함할 경우, KaTeX가 인식하도록 전처리 */
   const tabularParsed = parseTabularForKatex(numberingRemoved);
 
   return tabularParsed;
